f30b284b52034abdd34896f73cf39fc91c2d52cd
Mathias Gruber <gruber.mathias@gmail.com>
2021-11-03 21:22:09 +0100

    Refactoring of all TapDev methods. New organization is by family.

--- bmt/compiler.h
+++ bmt/compiler.h
@@ -6,6 +6,7 @@
 #define OPTIMIZED	__attribute__((optimize(2)))
 #define DEBUGGABLE 	__attribute__((optimize(0)))
 #define ALIGNED		__attribute__((aligned(4)))
+#define DEPRECATED	__attribute__((deprecated))
 
 #define _countof(t)		(sizeof(t)/sizeof(t[0]))
 
