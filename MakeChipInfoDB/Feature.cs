3d5bf2359103d98d595490b9647434664d34e9e7
grumat <gruber.mathias@gmail.com>
2021-12-08 09:25:19 +0100

    New Chip-info-extractor written in C#

---
+++ MakeChipInfoDB/Feature.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MakeChipInfoDB
+{
+	class Feature
+	{
+		public string Id;
+		public string ClockSystem;
+		public bool Lcfe;
+		public bool QuickMemRead;
+		public bool I2C;
+		public bool StopFllDbg;
+		public bool HasFram;
+
+		public Feature(featuresType o, Dictionary<string, Feature> refs)
+		{
+			if (o.id != null)
+				Id = MyUtils.MkIdentifier(o.id);
+			if (o.@ref != null)
+			{
+				// Merge ref immediately
+				string k = MyUtils.MkIdentifier(o.@ref);
+				Copy(refs[k]);
+			}
+			if(o.clockSystemSpecified)
+				ClockSystem = Enum.GetName(typeof(clockSystemType), o.clockSystem);
+			if (o.lcfeSpecified)
+				Lcfe = o.lcfe != Bool.@false;
+			if (o.quickMemReadSpecified)
+				QuickMemRead = o.quickMemRead != Bool.@false;
+			if (o.i2cSpecified)
+				I2C = o.i2c != Bool.@false;
+			if (o.stopFllDbgSpecified)
+				StopFllDbg = o.stopFllDbg != Bool.@false;
+			if (o.hasFramSpecified)
+				HasFram = o.hasFram != Bool.@false;
+		}
+
+		void Copy(Feature o)
+		{
+			Id = o.Id;
+			ClockSystem = o.ClockSystem;
+			Lcfe = o.Lcfe;
+			QuickMemRead = o.QuickMemRead;
+			I2C = o.I2C;
+			StopFllDbg = o.StopFllDbg;
+			HasFram = o.HasFram;
+		}
+	}
+
+	class Features
+	{
+		public Dictionary<string, Feature> Items = new Dictionary<string, Feature>();
+
+		public void AddItem(Feature o)
+		{
+			Items.Add(o.Id, o);
+		}
+	}
+}
---
+++ MakeChipInfoDB/MemoryLayout.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MakeChipInfoDB
+{
+	class MemoryLayout
+	{
+		public string Id;
+		public string Ref;
+		public List<Tuple<string, string>> MemoryFields = new List<Tuple<string, string>>();
+
+		public MemoryLayout(memoryLayoutType node, string lid, MemoryLayouts lays, ref Memories col)
+		{
+			Id = lid;
+			if (node.@ref != null)
+				Ref = lays.ResolveID(GetIdentifier(node.@ref));
+			if (node.memory != null)
+			{
+				foreach (var ff in node.memory)
+				{
+					string clas = Enums.UnMapMemoryNameType(ff.name);
+					string mid = Memory.GetIdentifier(lid + '_' + clas);
+					Memory o = new Memory(ff, col.MemAliases_);
+					if (o.IsPure())
+						MemoryFields.Add(Tuple.Create(clas, o.Ref));
+					else
+					{
+						o.Id = mid;
+						mid = col.AddItem(o);
+						MemoryFields.Add(Tuple.Create(clas, mid));
+					}
+				}
+			}
+		}
+
+		public bool Equals(MemoryLayout o)
+		{
+			if (Ref != o.Ref
+				|| MemoryFields.Count != o.MemoryFields.Count)
+				return false;
+			// Compare lists
+			foreach (Tuple<string, string> l in MemoryFields)
+			{
+				bool found = false;
+				foreach (Tuple<string, string> r in o.MemoryFields)
+				{
+					if (l.Item1 == r.Item1
+						&& l.Item2 == r.Item2)
+					{
+						found = true;
+						break;
+					}
+				}
+				if (found == false)
+					return false;
+			}
+			return true;
+		}
+
+		public static string GetIdentifier(string id)
+		{
+			return "kLyt" + MyUtils.MkIdentifier(id);
+		}
+
+		public void DoHFile(StreamWriter fh, Memories mems)
+		{
+			fh.Write("\t{ " + MemoryFields.Count.ToString() + ",\t");
+			if (Ref != null)
+				fh.Write(Ref + " },\n");
+			else
+				fh.Write("kLytNone },\n");
+			foreach (var i in MemoryFields)
+			{
+				int tmp = mems.ToIdx(i.Item2);
+				fh.Write(String.Format("\t\t{{kClas{0}, {1}}},\n", i.Item1, tmp));
+			}
+		}
+	}
+
+	class MemoryLayouts
+	{
+		public Dictionary<string, MemoryLayout> Mems_ = new Dictionary<string, MemoryLayout>();
+		public Dictionary<string, string> MemAliases_ = new Dictionary<string, string>();
+		public List<string> Phys_ = new List<string>();
+
+		public string AddItem(MemoryLayout o)
+		{
+			var match = Mems_.FirstOrDefault(x => o.Equals(x.Value));
+			if (match.Key != null)
+			{
+				MemAliases_.Add(o.Id, match.Key);
+				return match.Key;
+			}
+			else
+			{
+				Mems_.Add(o.Id, o);
+				Phys_.Add(o.Id);
+				return o.Id;
+			}
+		}
+
+		public string ResolveID(string id)
+		{
+			if (MemAliases_.ContainsKey(id))
+				return MemAliases_[id];
+			return id;
+		}
+
+		public string AppendDeviceLayout(memoryLayoutType node, string id, ref Memories mems)
+		{
+			string lid = null;
+			if (node.id != null)
+				lid = MemoryLayout.GetIdentifier(node.id);
+			else if(id != null)
+				lid = MemoryLayout.GetIdentifier(id);
+			else
+				throw new InvalidDataException("Unable to determine an id for element");
+			MemoryLayout m = new MemoryLayout(node, lid, this, ref mems);
+			//m.Id = lid;
+			return AddItem(m);
+		}
+
+		public void DoHFile(StreamWriter fh, Memories mems)
+		{
+			fh.Write("\nenum LytIndexes : uint8_t\n{\n");
+			foreach (var n in Phys_)
+				fh.Write("\t" + n + ",\n");
+			fh.Write("\tkLytNone = 255\n};\n\n");
+			fh.Write("static constexpr const MemoryLayoutBlob msp430_lyt_set[] =\n{\n");
+			for (int i = 0; i < Phys_.Count; ++i)
+			{
+				string n = Phys_[i];
+				MemoryLayout o = Mems_[n];
+				fh.Write(string.Format("\t// {0}: {1}\n", i, n));
+				o.DoHFile(fh, mems);
+			}
+			fh.Write(@"};
+
+
+ALWAYS_INLINE static const MemoryLayoutInfo *GetLyt(uint8_t idx)
+{
+	uint32_t c = 0;
+	// Iterate the list
+	for (uint8_t i = 0; i < idx; ++i)
+		c += msp430_lyt_set[c].low_ + 1;	// +1 for the header entry
+	return (const MemoryLayoutInfo *)&msp430_lyt_set[c];
+}
+
+");
+		}
+	}
+}
