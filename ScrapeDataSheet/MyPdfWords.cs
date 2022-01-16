7ef3f8b0dd23ffdcd56fefff9d4d82ae112be00c
grumat <gruber.mathias@gmail.com>
2022-01-16 21:09:13 +0100

    C# Tool to scrape all TI MSP430 datasheets and retrieve Flash + JTAG parameters into CSV files.

---
+++ ScrapeDataSheet/MyPdfWords.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Tabula;
+using Tabula.Detectors;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.Core;
+using UglyToad.PdfPig.DocumentLayoutAnalysis;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.PageSegmenter;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.WordExtractor;
+
+namespace ScrapeDataSheet
+{
+	static class MyPdfWords
+	{
+		public static IEnumerable<TextBlock> Extract(PdfDocument document, int pg_num)
+		{
+			Page page = document.GetPage(pg_num);
+			List<Word> words = NearestNeighbourWordExtractor.Instance.GetWords(page.Letters).ToList();	// extract words
+			if (pg_num == 1)
+				return RecursiveXYCut.Instance.GetBlocks(words);										// extract blocks and lines
+			return DefaultPageSegmenter.Instance.GetBlocks(words);										// extract blocks and lines
+		}
+
+		public static (PageArea, List<TableRectangle>) GetTables(PdfDocument document, int pg_num)
+		{
+			ObjectExtractor oe = new ObjectExtractor(document);
+			PageArea page = oe.ExtractPage(pg_num);
+
+			// detect candidate table zones
+			SimpleNurminenDetectionAlgorithm detector = new SimpleNurminenDetectionAlgorithm();
+			List<TableRectangle> regions = detector.Detect(page);
+			return (page, regions);
+		}
+	}
+}
---
+++ ScrapeDataSheet/Program.cs
@@ -0,0 +1,60 @@
+﻿//#define SINGLE_FILE
+
+using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Globalization;
+using System.IO;
+using static ScrapeDataSheet.ExtractFlashData;
+
+namespace ScrapeDataSheet
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string base_path = @"D:\[CDCache]\[Electronics]\[Embedded]\[µC]\Texas Instruments\MSP430\DataSheet";
+			CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
+			{
+				Mode = CsvMode.Escape,
+				Delimiter = "\t"
+			};
+			Console.WriteLine("Scrape TI Datasheet (Flash Info)");
+			using (FileStream fs = new FileStream("Flash.csv", FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+			using (StreamWriter writer = new StreamWriter(fs))
+			using (var csv = new CsvWriter(writer, config))
+			{
+#if SINGLE_FILE
+				string[] files = {
+					@"D:\[CDCache]\[Electronics]\[Embedded]\[µC]\Texas Instruments\MSP430\DataSheet\MSP430FR5xx Family\slase66c - Datasheet - MSP430fr597x(1), MSP430fr592x(1), MSP430fr587x(1).pdf",
+				};
+#else
+				string[] files = Directory.GetFiles(base_path, "*.pdf", SearchOption.AllDirectories);
+#endif
+				csv.WriteHeader<DocProperties>();
+				csv.NextRecord();
+				foreach (string test_file in files)
+				{
+					ExtractFlashData scrapper = new ExtractFlashData();
+					DocProperties info = scrapper.ScrapePdf(test_file, base_path);
+					if (info != null)
+					{
+						string[] chips = info.Chip.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+						Array.Sort(chips, StringComparer.InvariantCulture);
+						foreach (string chip in chips)
+						{
+							info.Chip = chip;
+							csv.WriteRecord(info);
+							csv.NextRecord();
+						}
+					}
+					else
+					{
+						writer.Flush();
+						fs.Flush();
+					}
+				}
+			}
+		}
+	}
+}
