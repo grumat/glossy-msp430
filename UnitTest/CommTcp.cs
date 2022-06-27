fd6364327c4c00e8fc1415fa723501f20442dbbc
Mathias Gruber <gruber.mathias@gmail.com>
2022-06-27 18:34:50 +0200

    Source code documentation

--- UnitTest/CommTcp.cs
+++ UnitTest/CommTcp.cs
@@ -13,68 +13,96 @@ namespace UnitTest
 {
 	internal class CommTcp : IComm
 	{
+		/// Opens a connection on the localhost for the given port to access gdbproxy-like interface
 		public CommTcp(int port)
 		{
+			// Localhost
 			IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+			// IP+port
 			IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
 			// Create a TCP/IP  socket.  
 			sender_ = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-			// First attempt needs more time for VCC initialization
+			// The first connection attempt sets a 10 seconds timeout as MSP-FET will be very slow to setup VCC
 			sender_.ReceiveTimeout = 10000;
+			// This connects TCP; gdbproxy also connects to MSP430.dll and initializes MCU
 			sender_.Connect(remoteEP);
-			if (sender_.RemoteEndPoint != null)
-				Console.WriteLine("Socket connected to {0}", sender_.RemoteEndPoint.ToString());
+			// Expecting an exception, if connect fails; why is object not initialized?
+			Debug.Assert(sender_.RemoteEndPoint != null);
+			// UI Feedback
+			Console.WriteLine("Socket connected to {0}", sender_.RemoteEndPoint.ToString());
 			// Wait until JTAG connection happens
 			Thread.Sleep(2000);
+			// A starting ACK is required
+			SendAck();
 		}
+		/// Destructor
 		~CommTcp()
 		{
+			// Disconnect send/rcv streams
 			sender_.Shutdown(SocketShutdown.Both);
+			// Close socket
 			sender_.Close();
 		}
+		/// Identifies platform; used to trim target behavior
 		public Platform GetPlatform()
 		{
 			return Platform.gdbproxy;
 		}
+		/// Sends an ACK, confirming the last reception is valid
 		public void SendAck()
 		{
 			sender_.Send(new byte[] { (byte)'+' });
 		}
-		public void SendNack()
+		/// Sends a NAK
+		public void SendNak()
 		{
 			sender_.Send(new byte[] { (byte)'-' });
 		}
+		/// Sends a message to the GDB. The message will be escaped by this method before transmitting
 		public int Send(string msg)
 		{
 			GdbOutBuffer proto = new GdbOutBuffer(msg);
 			// Send the data through the socket.
 			return sender_.Send(proto.MakePacket());
 		}
+		/// Retrieves a raw single byte from the input stream
 		public int Get()
 		{
+			// All buffered data retrieved?
 			if(max_ <= pos_)
 			{
 				try
 				{
+					// Receive next buffer
 					max_ = sender_.Receive(rcvbuf_);
 					// After first attempt reduce timeout
-					if (sender_.ReceiveTimeout == 10000)
+					if (sender_.ReceiveTimeout > 2000)
 						sender_.ReceiveTimeout = 2000;
 				}
 				catch(SocketException e)
 				{
+					// Rethrow if not a timeout
 					if (e.ErrorCode != 10060)
 						throw;
+					// Clear buffer
 					max_ = 0;
+					pos_ = 0;
+					// No char received
 					return -1;
 				}
+				// Set index to buffer head
 				pos_ = 0;
 			}
+			// Retrieve current byte and increment index
 			return rcvbuf_[pos_++];
 		}
+		// The socket object
 		protected Socket sender_;
+		// A tall buffer to receive data
 		protected byte[] rcvbuf_ = new byte[4096];
+		// Total bytes on buffer
 		protected int max_ = 0;
+		// Current buffer index
 		protected int pos_ = 0;
 	}
 }
