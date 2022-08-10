279d733ac14ef07d4125f0c192fcce4508fd87f7
grumat <gruber.mathias@gmail.com>
2022-08-10 21:52:35 +0200

    Added test profile for MSP430F247

--- GDB430/drivers/TapMcu.cpp
+++ GDB430/drivers/TapMcu.cpp
@@ -33,7 +33,7 @@ bool TapMcu::Open()
 	breakpoints_.ctor();
 
 #if OPT_JTAG_SPEED_SEL
-	g_Player.itf_ = &jtag_device_3;
+	g_Player.itf_ = &jtag_device_5;
 #else
 	g_Player.itf_ = &jtag_device;
 #endif
