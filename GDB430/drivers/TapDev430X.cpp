53c9e802d87a558321ee4384cb480d1e671abf29
Mathias Gruber <gruber.mathias@gmail.com>
2022-09-30 17:55:56 +0200

    New version of SingleStep method - WIP

--- GDB430/drivers/TapDev430X.cpp
+++ GDB430/drivers/TapDev430X.cpp
@@ -860,14 +860,4 @@ void TapDev430X::UpdateEemBreakpoints(Breakpoints &bkpts, const ChipProfile &pro
 /* CPU FLOW CONTROL                                                                   */
 /**************************************************************************************/
 
-// Source: uif
-bool TapDev430X::SingleStep(CpuContext &ctx, const ChipProfile &prof, uint16_t mdbval)
-{
-	// Stores BKPT 0 information
-	BkptSetting bkpt0;
-	// Preserve breakpoint block 0
-	ReadBkptSettings(bkpt0, 0, true);
-	
-	return false;
-}
 
