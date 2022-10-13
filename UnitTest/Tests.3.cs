a9e1a3ef72380f14833f84e232f61780fc36c1af
grumat <gruber.mathias@gmail.com>
2022-10-13 20:23:47 +0200

    Test 3 (WIP)

--- UnitTest/Tests.3.cs
+++ UnitTest/Tests.3.cs
@@ -8,5 +8,28 @@ namespace UnitTest
 {
 	internal partial class Tests : TestsBase
 	{
+
+		// Test 3 runs a simple funclet
+		private bool Test3()
+		{
+			// 100
+			if (!GetFeatures())
+				return false;
+			// 110
+			if (!GetReplyMode())
+				return false;
+			// 120
+			if (!StartNoAckMode())
+				return false;
+			// 220
+			if (!GetRegisterValues())
+				return false;
+
+
+			bool res = true;
+			if (!Detach())
+				res = false;
+			return res;
+		}
 	}
 }
