86c0e13218f0c52194d873bff35194965054338f
Mathias Gruber <gruber.mathias@gmail.com>
2023-12-24 19:33:42 +0100

    Fixed TimDmaWav and TmsAutoShaper classes.

--- target.bluepill/platform.h
+++ target.bluepill/platform.h
@@ -28,8 +28,8 @@ using namespace Bmt::Gpio;
 /// Timer used for TMS generation
 static constexpr Timer::Unit kJtmsShapeTimer = Timer::kTim1;		// Timer 1
 /// External clock source from SPI
-static constexpr Timer::ExtClk kJtmsTimerClk = Timer::ExtClk::kTI1FP1; // Timer Input 1 (PA8)
-//! PA9 (TIM1:TIM1_CH3) is used as output pin
+static constexpr Timer::ExtClk kJtmsTimerClk = Timer::ExtClk::kTI1F_ED; // Timer Input 1 (PA8)
+//! PA10 (TIM1:TIM1_CH3) is used as output pin
 static constexpr Timer::Channel kTmsOutChannel = Timer::Channel::k3;
 
 
